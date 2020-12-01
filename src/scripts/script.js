438c3de593e6aa91f846ecf5355bf324f91cbf71
Nikita <main.acr0matic@gmail.com>
2020-12-01 16:04:47 +0300

    feat: сверстан слайдер в "коворкинг"

---
+++ src/scripts/script.js
@@ -0,0 +1,1 @@
+const lightbox = GLightbox();
---
+++ src/scripts/slider.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.coworking-slider', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.coworking-slider__next',
+    prevEl: '.coworking-slider__prev',
+  },
+})
