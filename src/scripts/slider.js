6be875cdbff6c2d0cee8ba288ff9e8645544e1f9
Nikita <main.acr0matic@gmail.com>
2020-12-02 03:53:57 +0300

    feat: верстка "экскурсия"

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.coworking-slider', {
+var coworkingSlider = new Swiper('.coworking-slider', {
   slidesPerView: 2,
   spaceBetween: 30,
 
@@ -6,5 +6,16 @@ var mySwiper = new Swiper('.coworking-slider', {
   navigation: {
     nextEl: '.coworking-slider__next',
     prevEl: '.coworking-slider__prev',
+  },
+})
+
+var excursionSlider = new Swiper('.excursion-slider', {
+  slidesPerView: 1,
+  spaceBetween: 15,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.excursion-slider__next',
+    prevEl: '.excursion-slider__prev',
   },
 })
