cfd5c755f136d24546b805dd8d45892b43d8e88e
Nikita <main.acr0matic@gmail.com>
2020-12-06 17:09:52 +0300

    feat: верстка страницы 404

---
+++ src/404.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+  <!-- Базовые мета-теги для поисковиков -->
+  <title>Заголовок</title>
+
+  <!-- Иконки для страницы -->
+  <link rel="shortcut icon" href="img/favicons/favicon.ico" type="image/x-icon">
+  <link rel="icon" sizes="16x16" href="img/favicons/favicon-16x16.png" type="image/png">
+  <link rel="icon" sizes="32x32" href="img/favicons/favicon-32x32.png" type="image/png">
+  <link rel="apple-touch-icon-precomposed" href="img/favicons/apple-touch-icon-precomposed.png">
+  <link rel="apple-touch-icon" href="img/favicons/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="167x167" href="img/favicons/apple-touch-icon-167x167.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon-180x180.png">
+  <link rel="apple-touch-icon" sizes="1024x1024" href="img/favicons/apple-touch-icon-1024x1024.png">
+
+  <!-- Метатаги которые выводят информацию о странице в поисковой запрос -->
+  <meta name="description" content="описание не длиннее 155 символов" />
+  <meta name="keywords" content="мета-теги, шаблон, html, css, acr0matic" />
+
+  <!-- Метатаги для ссылок в социальных сетях -->
+  <meta property="og:locale" content="ru_RU" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="" />
+  <meta property="og:description" content="" />
+  <meta property="og:image" content="" />
+  <meta property="og:url" content="" />
+  <meta property="og:site_name" content="" />
+
+  <!-- Контролирует поведение поисковых систем при индексации страницы -->
+  <meta name="robots" content="index,follow,noodp" />
+
+  <!-- Покраска адресной строки в мобильных Chrome, Firefox OS и Opera -->
+  <meta name="theme-color" content="#4285f4" />
+
+  <!-- Для Windows Phone -->
+  <meta name="msapplication-navbutton-color" content="#4285f4" />
+
+  <!-- Для iOS Safari -->
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+  <!-- Верификация Яндекс.Вебмастер  -->
+  <meta name="yandex-verification" content="" />
+
+  <!-- Yandex.Metrika counter -->
+
+  <!-- /Yandex.Metrika counter -->
+
+  <!-- Стили -->
+  <!-- build:css -->
+  <link rel="stylesheet" href="css/style.css" />
+  <!-- endbuild -->
+</head>
+
+<body>
+
+  <!-- Шапка -->
+  <?php include_once 'partials/header.php' ?>
+
+  <!-- Основной контент -->
+  <main>
+    <section id="not-found">
+      <div class="not-found__container container">
+        <div class="not-found">
+          <div class="not-found__background"></div>
+
+          <div class="not-found__wrapper">
+            <div class="not-found__error">
+              <span class="not-found__notice">Ой!</span>
+              <span class="not-found__code">404</span>
+            </div>
+            <!-- /.404__error -->
+            <h1 class="not-found__title">Эта страничка еще в разработке или ее не существует ;)</h1>
+
+            <button class="not-found__button button button-outline">Вернуться на главную</button>
+          </div>
+          <!-- /.not-found__wrapper -->
+
+          <img class="not-found__image" src="img/pictures/404/notebook.svg" alt="">
+        </div>
+        <!-- /.404 -->
+      </div>
+      <!-- /.container -->
+    </section>
+  </main>
+
+  <!-- Подвал -->
+  <?php include_once 'partials/footer.php' ?>
+
+</body>
+
+</html>
