8f65752ed32b8b7e4e672ab51eef29b9a716c286
Nikita <main.acr0matic@gmail.com>
2020-12-28 19:49:02 +0300

    feat: сделан шаблон объекта

--- src/scripts/stl.js
+++ src/scripts/stl.js
@@ -1,7 +1,47 @@
-const teeths = document.querySelectorAll('.stl-teeth__item');
+// const teethData = [
+//   {
+//     'id': 41,
+//     'isSupport' : false,
+//     'stage': [
+//       {
+//         'stage': 1,
+//         'construction': 'Не определено',
+//         'system': 'Не определено',
+//         'material': 'Не определено',
+//         'parameter': 'Не определено',
+//         'design': 'Не определено',
+//         'advanced': 'Не определено',
+//       },
+//       {
+//         'stage': 2,
+//         'construction': 'Не определено',
+//         'system': 'Не определено',
+//         'material': 'Не определено',
+//         'parameter': 'Не определено',
+//         'design': 'Не определено',
+//         'advanced': 'Не определено',
+//       },
+//     ]
+//   }
+// ];
+
+const teethData = [
+  {
+
+  },
+];
+
+const teeths = document.querySelectorAll('.stl-teeth__map .stl-teeth__item');
+const teethPreviewContainer = document.querySelector('.stl-stage__teeth');
 
 teeths.forEach(teeth => {
+  // Прописываем айди каждому зубу на основе текста внутри зуба
+  teeth.setAttribute('data-teeth-id', teeth.querySelector('.stl-teeth__id').innerHTML.replace(/\s/g, ''));
   teeth.addEventListener('click', () => {
+    
+    // Очищаем блок и добавляем иконку зуба
+    teethPreviewContainer.innerHTML = '';
+    teethPreviewContainer.append(teeth);
     stlSlider.slideNext();
   });
 });
